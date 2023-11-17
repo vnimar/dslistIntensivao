3272c3c9b5113e53fc8771c9ea1f251e7e64b81e
Marcos Vinicius <vnimar.lima@gmail.com>
2023-11-16 21:50:09 -0300

    Project created

---
+++ src/main/java/com/vnimar/intensivao/IntensivaoApplication.java
@@ -0,0 +1,13 @@
+package com.vnimar.intensivao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntensivaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntensivaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vnimar/intensivao/IntensivaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnimar.intensivao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntensivaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
